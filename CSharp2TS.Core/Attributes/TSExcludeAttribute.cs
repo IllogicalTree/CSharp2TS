3d5f3609bb5f4e7227b479b7eb1652dd88800892
Sam Orme <sam@orme.me>
2025-03-30 18:19:18 +0100

    Allow exclude method to be added to methods

--- CSharp2TS.Core/Attributes/TSExcludeAttribute.cs
+++ CSharp2TS.Core/Attributes/TSExcludeAttribute.cs
@@ -1,5 +1,5 @@
 ﻿namespace CSharp2TS.Core.Attributes {
-    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false, Inherited = false)]
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Method, AllowMultiple = false, Inherited = false)]
     public class TSExcludeAttribute : Attribute {
     }
 }
