822bd124b117c32fb9ece2979684e4ae9839c9f7
Sam Orme <sam@orme.me>
2025-04-06 18:32:24 +0100

    Ignore nullable warning

--- CSharp2TS.Tests/Stubs/Models/TestClass.cs
+++ CSharp2TS.Tests/Stubs/Models/TestClass.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Concurrent;
+﻿#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
+using System.Collections.Concurrent;
 using System.Collections.ObjectModel;
 using CSharp2TS.Core.Attributes;
 
@@ -92,3 +93,5 @@ namespace CSharp2TS.Tests.Stubs.Models {
         //public ConcurrentBag<int?> NullableIntConcurrentBag { get; set; }
     }
 }
+
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
