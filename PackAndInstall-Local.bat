bb55fe5dff669a01e4f0651dd9d5ec19816ae3b7
Sam Orme <sam@orme.me>
2025-04-11 16:57:56 +0100

    Add prerelease flag as the package generated at the moment is in prerelease

--- PackAndInstall-Local.bat
+++ PackAndInstall-Local.bat
@@ -1,5 +1,5 @@
 call dotnet build -c Release
 call dotnet pack -c Release
 dotnet tool uninstall -g csharp2ts.cli
-dotnet tool install -g --add-source ./nupkg csharp2ts.cli
+dotnet tool install -g --prerelease --add-source ./nupkg csharp2ts.cli
 pause
