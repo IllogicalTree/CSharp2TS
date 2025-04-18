4c0dd8dd2913589c8448b1bcf229c193e2fbc211
Sam Orme <Saor@salus-technical.com>
2025-04-18 15:06:49 +0100

    Remove temp args

--- CSharp2TS.CLI/Program.cs
+++ CSharp2TS.CLI/Program.cs
@@ -4,8 +4,6 @@ using System.Text.Json;
 namespace CSharp2TS.CLI {
     public class Program {
         private static void Main(string[] args) {
-            args = ["-c", "C:\\Working\\SalusSuite\\master\\AdminPortal\\csharp2ts.json"];
-
             if (args.Length == 0) {
                 ShowIntro();
                 return;
